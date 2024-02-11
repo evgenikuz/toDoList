b701f338e14bee9c5058ba78379320abc15d2a7e
Evgeniia <kuznetsovaevgeni@gmail.com>
2024-02-11 13:56:57 +0300

    local storage, повесила submit на форму

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
 let submit = document.querySelector('.submit-btn');
 let taskInput = document.querySelector('.input-form');
+let form = document.querySelector('form');
 const tasksList = document.querySelector('.tasks');
 const deleteBlock = document.querySelector('.delete-block');
 let deleteAll = document.querySelector('.delete-block__delete-all');
 let deleteChecked = document.querySelector('.delete-block__delete-checked');
+let localStorage = window.localStorage;
 let taskCounter = 0;
+let arr = [];
 
-taskInput.onkeypress = function(e) {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-        if (taskInput.value) {
-            taskInput.classList.remove('error')
-            if (taskCounter === 0) {
-                tasksList.classList.remove('d-none');
-                deleteBlock.classList.remove('d-none');
-                addTask();
-            } else {
-                addTask();
-            }
-        } else {
-            taskInput.classList.add('error')
-        }
-    }
-}
-submit.addEventListener('click', function(e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (taskInput.value) {
         taskInput.classList.remove('error')
         if (taskCounter === 0) {
             tasksList.classList.remove('d-none');
             deleteBlock.classList.remove('d-none');
-            addTask();
+            addTask(taskInput.value);
         } else {
-            addTask();
+            addTask(taskInput.value);
         }
     } else {
         taskInput.classList.add('error')
     }
 })
 
-function addTask() {
+function addTask(text) {
     taskCounter++;
     const task = document.createElement('div');
     task.className = "task";
-
+    
+    let firstUpperLetter = (str) => str.split('')[0].toUpperCase()+str.slice(1);
+    arr.push(firstUpperLetter(text.trim()));
+    localStorage.setItem('task', arr);
     const taskCheckbox = document.createElement('input');
     taskCheckbox.className = "task__checkbox";
     taskCheckbox.setAttribute("type", "checkbox");
@@ -52,7 +41,7 @@ function addTask() {
     const labelForCheckbox = document.createElement('label');
     labelForCheckbox.className = "task__text";
     labelForCheckbox.setAttribute("for", taskCounter);
-    labelForCheckbox.textContent = taskInput.value;
+    labelForCheckbox.textContent = text;
 
     const deleteButton = document.createElement('button');
     deleteButton.className = "task__delete-btn";
@@ -77,7 +66,6 @@ function addTask() {
         }
     })
 
-
     deleteButton.addEventListener('click', function() {
         task.remove();
         taskCounter--;
@@ -90,13 +78,36 @@ function addTask() {
     deleteAll.addEventListener('click', function() {
         task.remove();
         taskCounter = 0;
+        localStorage.removeItem('task')
         tasksList.classList.add('d-none');
         deleteBlock.classList.add('d-none');
     })
     deleteChecked.addEventListener('click', function() {
+        localStorage.getItem('task');
         if(taskCheckbox.checked) {
+            taskCheckbox.getAttribute('id');
             task.remove()
+            // taskCounter--;
         }
+        // if (!task) {
+        //     tasksList.classList.add('d-none');
+        //     deleteBlock.classList.add('d-none');
+        // }
+        // console.log(taskCounter)
+        // if (taskCounter == 0) {
+        //     tasksList.classList.add('d-none');
+        //     deleteBlock.classList.add('d-none');
+        // }
     })
+}
+if(localStorage.task) {
+    let localStorageArray = localStorage.task.split(',');
+    if (taskCounter === 0) {
+        tasksList.classList.remove('d-none');
+        deleteBlock.classList.remove('d-none');
+    }
+    for(let el of localStorageArray) {
+        addTask(el);
+    }
 }
 
