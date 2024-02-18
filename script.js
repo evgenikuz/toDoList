9f6bc6ded5758105f477a7c97e1a340699a37eed
Evgeniia <kuznetsovaevgeni@gmail.com>
2024-02-18 13:16:00 +0300

    переделала скрипт

--- script.js
+++ script.js
@@ -1,152 +1,129 @@
-let submit = document.querySelector('.submit-btn');
 let taskInput = document.querySelector('.input-form');
 let form = document.querySelector('form');
 const tasksList = document.querySelector('.tasks');
 const deleteBlock = document.querySelector('.delete-block');
-let deleteAll = document.querySelector('.delete-block__delete-all');
-let deleteChecked = document.querySelector('.delete-block__delete-checked');
 let localStorage = window.localStorage;
 let taskCounter = 0;
-let arr = [];
+let taskArr = []; // массив с которым работаем, копия того что в local storage
+let list = document.querySelector('.list')
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (taskInput.value) {
         taskInput.classList.remove('error')
+        let firstUpperLetter = (str) => str.split('')[0].toUpperCase()+str.slice(1);
         if (taskCounter === 0) {
             tasksList.classList.remove('d-none');
             deleteBlock.classList.remove('d-none');
-            addTask(taskInput.value);
+            addTask(firstUpperLetter(taskInput.value.trim()));
         } else {
-            addTask(taskInput.value);
+            addTask(firstUpperLetter(taskInput.value.trim()));
         }
     } else {
         taskInput.classList.add('error')
     }
 })
 
-function addTask(text) {
+function addTask(text, isChecked = false) { //создание заданий
     taskCounter++;
     const task = document.createElement('div');
     task.className = "task";
     
-    let firstUpperLetter = (str) => str.split('')[0].toUpperCase()+str.slice(1);
-    arr.push({taskText: firstUpperLetter(text.trim()), id: taskCounter, isChecked: false});
-    localStorage.setItem('task', JSON.stringify(arr));
+    taskArr.push({taskText: text, id: taskCounter, isChecked: isChecked});
+    localStorage.setItem('task', JSON.stringify(taskArr));
 
-    const label = document.createElement('label');
-    label.innerHTML = `
-    <input type="checkbox" class="task__checkbox visually-hidden" id="${taskCounter}">
-    <span class="custom-checkbox"></span>
-    <p for="${taskCounter}" class="task__text">${text}</p>        
-    `;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.className = "task__delete-btn";
-    deleteButton.textContent = "❌";
-
-    tasksList.append(task)
-    task.append(label, deleteButton);
-    taskInput.value = "";
+    task.innerHTML = `
+    <label>
+        <input type="checkbox" class="task__checkbox visually-hidden" id="${taskCounter}">
+        <span class="custom-checkbox"></span>
+        <p class="task__text">${text}</p>        
+    </label>
+    <button class="task__delete-btn">❌</button>`
     
+    tasksList.append(task)
+    taskInput.value = "";
 }
-// если существует local storage
-if(localStorage.task) {
+if(localStorage.task) { // если существует local storage
     // открываем список и кнопки удаления
     tasksList.classList.remove('d-none');
     deleteBlock.classList.remove('d-none');
-    //превращаем local storage в массив
-    let localStorageArray = JSON.parse(localStorage.task.split(','));
     // отображаем список из хранилища local storage
+    let localStorageArray = JSON.parse(localStorage.task.split(','));
     for(let el of localStorageArray) {
-        addTask(el.taskText);
-    }
-    // еще раз находим task text и task checkbox (на выходе массивы)
-    const labelForCheckbox = document.querySelectorAll('.task__text');
-    const taskCheckbox = document.querySelectorAll('.task__checkbox');
-    const task = document.querySelectorAll('.task') // task (массив)
-    const label = document.querySelectorAll('label')
-    // добавляем event listener к каждому пункту списка и стиль checked
-    for (let i = 0; i < task.length; i++) {
-        label[i].addEventListener('click', function(event) {
-            if (event.target.className.contains('custom-checkbox') || event.target.className.contains('task__text')) {
-                labelForCheckbox[i].classList.toggle('checked')
-                let toCheckId = taskCheckbox[i].getAttribute('id');
-                console.log(toCheckId);
-                for (obj of localStorageArray) {
-                    if (obj.id === +toCheckId) {
-                        // т.к. проверка происходит до нажатия, то логика обратная (если checked до нажатия, то !checked после)
-                        if(taskCheckbox[i].checked) {
-                        // меняем в local storage isChecked у элемента
-                            obj.isChecked = false;
-                        } else {
-                        // меняем в local storage isChecked у элемента
-                        obj.isChecked = true;
-                        }
-                        // записываем в local storage новые данные
-                        localStorage.setItem('task', JSON.stringify(localStorageArray));
-                    }
-                }
-            }
-        })
-    }
-// еще раз находим все кнопки-крестики (массив)
-    const deleteButton = document.querySelectorAll('.task__delete-btn');
-    for (let i = 0; i < deleteButton.length; i++) {
-    deleteButton[i].addEventListener('click', function() {
-        // получаем id удаляемой записи
-        let idOfDeletedTask = taskCheckbox[i].getAttribute('id');
-        for (obj of localStorageArray) {
-            if (obj.id === +idOfDeletedTask) {
-                // вырезаем то что удалили
-                localStorageArray.splice(localStorageArray.indexOf(obj), 1);
-                // задаем новое значение local storage
-                localStorage.setItem('task', JSON.stringify(localStorageArray))
-            }
+        addTask(el.taskText, el.isChecked);
+        if (el.isChecked) { // добавляем отмеченность в случае, если задание было отмечено до обновления
+            document.querySelector('.task__text').classList.add('checked');
+            document.querySelector('.task__checkbox').checked = true;
         }
-        task[i].remove();
-        taskCounter--;
-        if (taskCounter === 0) {
-            tasksList.classList.add('d-none');
-            deleteBlock.classList.add('d-none');
-            // чистим local storage от пустого массива
-            localStorage.clear();
-        }
-    })
+    }
 }
-    // еще раз объявляем кнопку "Удалить все"
-    const deleteAll = document.querySelector('.delete-block__delete-all');
-    deleteAll.addEventListener('click', function() {
-        // удаляем со страницы все записи
-        for (el of task) {
-            el.remove();
+
+list.addEventListener('click', function(event) { // слушаем событие клика на весь лист, и дальше определяем где он и включаем функцию
+    if (event.target.classList.contains('task__checkbox' || 'custom-checkbox' || 'task__text')) { // если нажали на задание
+        toCheckId = event.target.closest('.task').querySelector('.task__checkbox').getAttribute('id');
+        event.target.closest('.task').querySelector('.task__text').classList.toggle('checked')
+        event.target.closest('.task').querySelector('.task__checkbox').checked ? checkTask(toCheckId, localStorage) : uncheckTask(toCheckId, localStorage); // отмечаем выполненным и не выполненным
+    } else if (event.target.classList.contains('task__delete-btn')){ // если нажали на крестик
+        let id = event.target.closest('.task').querySelector('.task__checkbox').getAttribute('id');
+        deleteTask(id, localStorage);
+        event.target.closest('.task').remove(); // удаляем задание
+        isLast(localStorage); // проверка на последний элемент в массиве
+    } else if (event.target.classList.contains('delete-block__delete-checked')){ // если нажали на удалить завершенные
+        let checkboxes = document.querySelectorAll('.task__checkbox');
+        for (el of checkboxes) {
+            if (el.checked) {
+             el.closest('.task').remove()
+            }
         }
-        taskCounter = 0;
-        localStorage.clear();
+        deleteChecked(localStorage);
+        isLast(localStorage); // проверка на последний элемент в массиве
+    } else if (event.target.classList.contains('delete-block__delete-all')){ // если нажали на удалить все
+        let tasks = document.querySelector('.tasks')
+        deleteAll(tasks)
+    }
+})
+
+function checkTask(id, localStorage) { // добавляем выполнение
+    for (obj of taskArr) {
+        if (obj.id === +id) {
+            obj.isChecked = true;
+            localStorage.setItem('task', JSON.stringify(taskArr))
+        }
+    }
+}
+function uncheckTask(id, localStorage) { // снимаем выполнение
+    for (obj of taskArr) {
+        if (obj.id === +id) {
+            obj.isChecked = false;
+            localStorage.setItem('task', JSON.stringify(taskArr))
+        }
+    }
+}
+function deleteTask(id, localStorage) { // удаляем задание по крестику
+    for (obj of taskArr) {
+        if (obj.id === +id) {
+            taskArr.splice(taskArr.indexOf(obj), 1)
+            localStorage.setItem('task', JSON.stringify(taskArr))
+        }
+    }
+}
+function isLast(localStorage) { // проверяем, не последний ли это элемент в списке и скрываем кнопки и поле с заданиями
+    if (taskArr.length === 0) {
         tasksList.classList.add('d-none');
         deleteBlock.classList.add('d-none');
-    })
-
-    deleteChecked.addEventListener('click', function() {
-        for (let i = 0; i < task.length; i++)
-        if(taskCheckbox[i].checked) {
-            let toDeleteId = taskCheckbox[i].getAttribute('id');
-            for(let obj of localStorageArray) {
-                if (obj.id === +toDeleteId) {
-                    // вырезаем то что удалили
-                    localStorageArray.splice(localStorageArray.indexOf(obj), 1);
-                    taskCounter--;
-                }
-                // задаем новое значение local storage
-                localStorage.setItem('task', JSON.stringify(localStorageArray))
-                task[i].remove()
-            }
-        }
-        if (taskCounter === 0) {
-            tasksList.classList.add('d-none');
-            deleteBlock.classList.add('d-none');
-            // чистим local storage от пустого массива
-            localStorage.clear();
-        }
-    })
+        localStorage.clear();
+        taskCounter = 0
+    }
+}
+function deleteChecked(localStorage) { // фильтруем массив по признаку не "isChecked = true"
+    taskArr = taskArr.filter(el => !el.isChecked)
+    localStorage.setItem('task', JSON.stringify(taskArr))
+}
+function deleteAll(tasks) { // удаляем со страницы все записи
+    tasks.innerHTML = '';
+    taskCounter = 0;
+    localStorage.clear();
+    taskArr = [];
+    tasksList.classList.add('d-none');
+    deleteBlock.classList.add('d-none');
 }
